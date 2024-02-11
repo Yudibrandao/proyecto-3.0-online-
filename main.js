307c0971c8a1d624ba990c22f80e4ef90d68295e
´Yudith´ <yudi@geeksacademy.es>
2024-02-11 22:46:16 +0100

    act.

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let trys = 8;
 let colors = ['blue', 'green', 'red', 'yellow', 'pink', 'aqua'];
 
 let random_code = [];
-let crackTry = 1; 
+let crackTry = 1;
 
 init();
 
@@ -19,16 +19,16 @@ function init() {
 
 
 
-    for( let i = 1; i<= trys; i++){
-        let div_try = document.createElement ('div');
-        div_try.setAttribute('id', 'try-'+i);
+    for (let i = 1; i <= trys; i++) {
+        let div_try = document.createElement('div');
+        div_try.setAttribute('id', 'try-' + i);
         div_try.setAttribute('class', 'try');
-        let div_left = document.createElement ('div');
+        let div_left = document.createElement('div');
         div_left.setAttribute('class', 'left');
         let div_right = document.createElement('div');
         div_right.setAttribute('class', 'right');
 
-        for (let i = 1; i <= codeLength; i++){
+        for (let i = 1; i <= codeLength; i++) {
             let div_l = document.createElement('div');
             let div_r = document.createElement('div');
             div_left.append(div_l);
@@ -41,21 +41,21 @@ function init() {
 
     }
 
-    for ( let i = 1; i<= codeLength; i++){
+    for (let i = 1; i <= codeLength; i++) {
         let div_select = document.createElement('div');
         div_select.setAttribute('class', 'select');
         let select = document.createElement('select');
 
-        for(let color of colors){
+        for (let color of colors) {
             let option = document.createElement('option');
-            option.setAttribute('style', 'background-color:'+color);
+            option.setAttribute('style', 'background-color:' + color);
             option.setAttribute('value', color);
             select.append(option);
         }
-        select.setAttribute('style', 'background-color:'+colors[0]);
+        select.setAttribute('style', 'background-color:' + colors[0]);
 
         select.addEventListener('change', (e) => {
-            e.target.setAttribute('style', 'background-color:'+e.target.value);
+            e.target.setAttribute('style', 'background-color:' + e.target.value);
 
         });
 
@@ -63,6 +63,31 @@ function init() {
         div_colors.append(div_select);
 
     }
+    createRandomCode();
 
 }
 
+function createRandomCode() {
+    for (let i = 1; i <= codeLength; i++) {
+        let random_color = colors[Math.floor(Math.random() * colors.length)]
+        random_code.push(random_color);
+
+    }
+
+}
+
+chose_button.addEventListener('click', (e) => {
+    let input_colors = document.querySelectorAll('.select>select');
+    let input_colors_arr = [];
+    for (let v of input_colors) {
+        input_colors_arr.push(v.value);
+    }
+    show('left', input_colors_arr);
+})
+
+function show(type, colors){
+    let tryView = document.querySelectorAll('#try-'+crackTry+'>.'+type+'>div');
+    tryView.forEach((v, i)=> {
+        v.setAttribute('style', 'background-color:'+colors[i]);
+    });
+}
